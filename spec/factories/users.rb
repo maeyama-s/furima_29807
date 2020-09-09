cb01519303c9f6a7cef4eebd10c23b1fb0972491
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-09 18:04:22 +0900

    mix_caseオプション削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6, mix_case: true) }
+    password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     family_name           { '前' }
     first_name            { '祥' }
