9cbedb993cde9db9ef8a6bf4313474c22a00c73e
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-10 18:02:21 +0900

    ActiveHash実装（配送料の負担）

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :item_condition
+  belongs_to_active_hash :shipping_charge
 
   validates :category_id, numericality: { other_than: 1 }
   validates :item_condition_id, numericality: { other_than: 1 }
+  validates :shipping_charges_id, numericality: { other_than: 1 }
 end
