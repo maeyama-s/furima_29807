077b9013b4d8da5d2cecffe99983ade47d71f3f9
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-10 15:49:43 +0900

    出品商品情報の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,17 @@ class ItemsController < ApplicationController
 
   #商品出品ページ
   def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :title, :explanation, :price)
   end
 
 end
