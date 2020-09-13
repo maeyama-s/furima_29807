54e7c6f955e9ae14163d90cd7f897acf5b18a006
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-13 11:14:54 +0900

    販売手数料、販売利益計算

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// JavaScriptファイルを読み込む記述
+require("./price")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
