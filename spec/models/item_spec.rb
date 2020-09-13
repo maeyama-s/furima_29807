f8eca1b85e0b78ae4503dc16e2448247a16d9fea
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-13 15:49:42 +0900

    テストコード（うまくいくとき）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload("#{::Rails.root}/spec/images/star.png")
+  end
+
+  describe '商品出品' do
+    context '商品出品がうまくいくとき' do
+      it '商品出品に必要なデータだあれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品がうまくいかないとき' do
+    end
+  end
 end
