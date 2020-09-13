83e5e45a5c24b2d78729b87aaac5ffd4df3b161a
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-13 16:30:20 +0900

    テストコード（うまくいかないとき）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,6 +14,68 @@ RSpec.describe Item, type: :model do
     end
 
     context '商品出品がうまくいかないとき' do
+      it 'ログインしていないユーザーは出品ページへ遷移できない' do
+      end
+      it '画像が空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空だと出品できない' do
+        @item.title = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+      it '商品の説明が空だと出品できない' do
+        @item.explanation = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'カテゴリーついての情報が空だと出品できない' do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category is not a number")
+      end
+      it '商品の状態についての情報が空だと出品できない' do
+        @item.item_condition_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition is not a number")
+      end
+      it '発送料の負担ついての情報が空だと出品できない' do
+        @item.shipping_charges_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charges is not a number")
+      end
+      it '発送元の地域ついての情報が空だと出品できない' do
+        @item.shipping_area_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area is not a number")
+      end
+      it '発送までの日数ついての情報が空だと出品できない' do
+        @item.days_until_shipping_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days until shipping is not a number")
+      end
+      it '価格ついての情報が空だと出品できない' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が、¥300未満だと出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '価格が、¥10,000,000以上だと出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+      end
+      it '価格は半角数字でないと出品できない' do
+        @item.price = "１０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
     end
   end
 end
