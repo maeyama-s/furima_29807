f5888f0c138c3980e2c24ba524e44e839ec23109
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-14 15:08:27 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :move_to_login, only: :new
+  before_action :authenticate_user!, only: :new
 
   # トップページ
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   # 商品出品ページ
@@ -23,14 +23,9 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :title, :explanation, :price, :category_id, :item_condition_id, :shipping_charges_id, :shipping_area_id, :days_until_shipping_id).merge(user_id: current_user.id)
-  end
-
-  def move_to_login
-    unless user_signed_in?
-      redirect_to new_user_session_path
-      # エラーメッセージをビューに渡す
-      flash[:notice] = 'You need to sign in or sign up before continuing.'
-    end
+    params.require(:item).permit(
+      :image, :title, :explanation, :price, :category_id, :item_condition_id,
+      :shipping_charges_id, :shipping_area_id, :days_until_shipping_id
+    ).merge(user_id: current_user.id)
   end
 end
