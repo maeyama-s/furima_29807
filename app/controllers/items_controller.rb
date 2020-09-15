2638d8cf80093f3155a84b8c0ea15a8e8f7d7568
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-15 15:14:15 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :authenticate_user, only: :edit
+  before_action :set_item, only: [:show, :edit, :update]
 
   # トップページ
   def index
@@ -21,15 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -39,10 +38,22 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(
       :image, :title, :explanation, :price, :category_id, :item_condition_id,
       :shipping_charges_id, :shipping_area_id, :days_until_shipping_id
     ).merge(user_id: current_user.id)
+  end
+
+  #権限のないユーザーが商品編集URLを入力した場合
+  def authenticate_user
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 end
