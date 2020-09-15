9c386dc2aeb5b5aec97ae4348b4506092ea14567
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-09-15 16:36:51 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   before_action :unauthorized, only: :edit
-  before_action :set_item, only: [:show, :update]
+  before_action :set_item, only: [:show, :update, :destroy]
 
   # トップページ
   def index
@@ -37,9 +37,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
