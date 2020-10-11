34b3933773229048e22f1b524fc725f72677835a
maeyama-s <70187409+maeyama-s@users.noreply.github.com>
2020-10-11 10:57:23 +0900

    タイムゾーンJSTに設定

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,10 @@ module Furima29807
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # タイムゾーン設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
+
